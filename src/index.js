256a82b7b3f26a4ea02bf4bf9efa8e5822bb21a6
BrendomGoncalves <brendom.vi@gmail.com>
2026-05-14 19:57:18 -0300

    adiciona configuração do banco de dados e atualiza dependências

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const database = require('./database/database');
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
+
+app.use(express.json());
+
+database.sync(() => {
+    console.log('Database synced: ', process.env.DATABASE);
+})
+
+app.listen(process.env.PORT_SERVER, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
