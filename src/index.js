52b48d33ba23b3a434a236f3a43ef3bf8bbc27bb
BrendomGoncalves <brendom.vi@gmail.com>
2026-05-15 17:53:38 -0300

    adiciona tratamento de erro na sincronização do banco de dados

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 database.sync(() => {
     console.log('Database synced: ', process.env.DATABASE);
-}).then(r => {
-    console.log('Database synced: ', r);
-})
+}).catch(error => {
+    console.log(error);
+});
 
 app.listen(process.env.PORT_SERVER, () => {
     console.log('Server is running on http://localhost:3000');
