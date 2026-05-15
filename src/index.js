e10debb80b2297d6db015f488b0fca75079e3e1f
BrendomGoncalves <brendom.vi@gmail.com>
2026-05-15 17:50:46 -0300

    adiciona log adicional após a sincronização do banco de dados

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 
 database.sync(() => {
     console.log('Database synced: ', process.env.DATABASE);
+}).then(r => {
+    console.log('Database synced: ', r);
 })
 
 app.listen(process.env.PORT_SERVER, () => {
